71d63d20c0b1909a4e0004e0527446aa0093565b
Double-gg123 <sharo.gibz@gmail.com>
2023-03-16 15:08:17 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
  /**
- * main - Entry point
+ * main - A programthat prints a line with puts function
  *
  * Return: Always 0 (Success)
- * /
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
