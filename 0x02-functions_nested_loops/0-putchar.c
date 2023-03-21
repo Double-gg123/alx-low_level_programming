70021b0160777552d38f18a2d7644ed85b471de7
Double-gg123 <sharo.gibz@gmail.com>
2023-03-21 12:09:25 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- *  main - Enty point
- *
- *  Return: Always 0 (Success)
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
-
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+int c = 0;
+char holberton[10] = "_putchar\n";
+
+while (c < 9)
+{
+_putchar(holberton[c]);
+c++;
+}
+return (0);
 }
