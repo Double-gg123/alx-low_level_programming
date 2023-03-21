68b31c36e7c2c3ad8829868181f706d1092d94f9
Double-gg123 <sharo.gibz@gmail.com>
2023-03-21 14:44:04 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,21 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		printf("+");
-		return (1);
-	}
-	else if (n == 0)
-	{
-		printf("0");
-		return (0);
-	}
-	else
-	{
-		printf("-");
-		return (-1);
-	}
+int result;
+if (n > 0)
+{
+_putchar('+');
+result = 1;
+}
+if (n == 0)
+{
+_putchar('0');
+result = 0;
+}
+if (n < 0)
+{
+_putchar('-');
+result = -1;
+}
+return (result);
 }
