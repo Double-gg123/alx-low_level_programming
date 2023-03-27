cf7ec186dd1a037dcb6e741048d94326e073f322
Double-gg123 <sharo.gibz@gmail.com>
2023-03-27 15:21:29 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,18 @@
  *
  *Return: void.
  */
-void puts2(char *str) {
-    int len = 0;
-    for (int i = 0; str[i] != '\0'; i++) {
-        len++;
-    }
-    for (int i = 0; i < len; i += 2) {
-        putchar(str[i]);
-    }
-    putchar('\n');
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
