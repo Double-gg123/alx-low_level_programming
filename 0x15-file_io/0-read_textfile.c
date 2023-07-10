30b88119a972c6a9179d0dd540fa1e91ec63476c
Double-gg123 <sharogibz.com>
2023-07-10 15:34:06 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output.
  * @filename: name of the file
  * @letters: number of letters
  *
